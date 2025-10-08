d5593613bac8a707ccb472f120434ad42fb71e8b
M.Hamza <152792061+hamzasand@users.noreply.github.com>
2025-10-08 15:59:43 +0500

    Update create_index.py

--- create_index.py
+++ create_index.py
@@ -9,8 +9,6 @@ from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 
-
-
 from dotenv import load_dotenv
 
 # Load environment variables from a .env file
