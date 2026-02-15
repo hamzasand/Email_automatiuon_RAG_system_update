1a7da4c93e00900b7fe46652235fe6f561809f08
M.Hamza <152792061+hamzasand@users.noreply.github.com>
2026-02-15 23:03:53 +0500

    Remove unnecessary blank lines in main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 #######################################
 # Used forCustomer query based Emails
-
 #######################################
 from colorama import Fore, Style
-
 from src.graph import Workflow
 
 from dotenv import load_dotenv
