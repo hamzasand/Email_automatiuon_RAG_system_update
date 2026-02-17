1cce0e6031278d66ed838778848739e864a33301
M.Hamza <152792061+hamzasand@users.noreply.github.com>
2026-02-17 21:49:06 +0500

    Add import for Workflow from src.graph

--- deploy_api.py
+++ deploy_api.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from langserve import add_routes
+
 from src.graph import Workflow
 from dotenv import load_dotenv
 
