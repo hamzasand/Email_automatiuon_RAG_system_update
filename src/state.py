b2708885da6eec48afe9f1f82804712ff3eb5e1e
M.Hamza <152792061+hamzasand@users.noreply.github.com>
2026-02-22 21:34:02 +0500

    Update state.py

--- src/state.py
+++ src/state.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel, Field
 from typing import List, Annotated
 from typing_extensions import TypedDict
-
 from langgraph.graph.message import add_messages
 
 class Email(BaseModel):
