c249de91ee4598e3cf4ffa24da43e16270acc4d4
M.Hamza <152792061+hamzasand@users.noreply.github.com>
2026-03-17 20:40:29 +0500

    Update create_index.py

--- create_index.py
+++ create_index.py
@@ -3,7 +3,6 @@
 ################################################
 from langchain_chroma import Chroma
 from langchain_core.prompts import ChatPromptTemplate
-
 from langchain_core.runnables import RunnablePassthrough
 
 from langchain_core.output_parsers import StrOutputParser
