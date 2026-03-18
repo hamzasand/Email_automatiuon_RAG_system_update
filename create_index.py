55aa97b125d484bf0858f984f1dec0ff535c832e
M.Hamza <152792061+hamzasand@users.noreply.github.com>
2026-03-18 15:51:26 +0500

    Update create_index.py

--- create_index.py
+++ create_index.py
@@ -2,7 +2,6 @@
 # Run this file for your domain specific Dataset
 ################################################
 from langchain_chroma import Chroma
-
 from langchain_core.prompts import ChatPromptTemplate
 
 from langchain_core.runnables import RunnablePassthrough
